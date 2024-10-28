da09c55562bad0b822dc2d4ef113df428f3f8e61
Ohas506 <142551067+Ohas506@users.noreply.github.com>
2024-10-27 20:55:29 -0700

    Create Code1.js

---
+++ Code1.js
@@ -0,0 +1,278 @@
+campaignTrail_temp.election_json = [
+    {
+        "model": "campaign_trail.election",
+        "pk": 3,
+        "fields": {
+            "year": 2012,
+            "summary": "<p>Incumbent president George HW Bush attempts to secure reelection while eccentric businessman Ross Perot has taken the Democratic party by storm. Key issues include the economy and trade policy.\n</p> <ul><li> With a sluggish economy, the unpopular Bush will have to exploit his opponents' unconventionality and unfamiliarity with national politics. \n</li><li>Charm and wit catapulted Perot to the nomination, but the general is a whole different ball game, and any slip up has the potential to send him crashing down.\n</li></ul>",
+            "image_url": "https://i.imgur.com/SrskX2S.jpeg",
+            "winning_electoral_vote_number": 270,
+            "advisor_url": "https://www.jetsimon.com/public/static/images/carville-rove.jpg",
+            "recommended_reading": "<h4 style=\"margin-top: 0.5em;\">Political News and Analysis</h4> <a href=\"http://www.realclearpolitics.com/\" target=\"_blank\">Real Clear Politics</a> -- <a href=\"http://www.theatlantic.com/\" target=\"_blank\">The Atlantic</a> -- <a href=\"http://www.politico.com/\" target=\"_blank\">Politico</a> -- <a href=\"http://www.huffingtonpost.com/\" target=\"_blank\">Huffington Post</a> -- <a href=\"http://www.foxnews.com/politics/index.html\" target=\"_blank\">Fox News Politics</a> <h4>Books by the Candidates</h4>Barack Obama - <a href=\"http://www.amazon.com/exec/obidos/ASIN/1400082773/amhius20-20\" target=\"_blank\">Dreams from My Father: A Story of Race and Inheritance</a><br>Barack Obama - <a href=\"http://www.amazon.com/exec/obidos/ASIN/0307455874/amhius20-20\" target=\"_blank\">The Audacity of Hope: Thoughts on Reclaiming the American Dream (Vintage)</a><br> Mitt Romney - <a href=\"http://www.amazon.com/exec/obidos/ASIN/0312671733/amhius20-20\" target=\"_blank\">No Apology: Believe in America</a><br> Gary Johnson - <a href=\"http://www.amazon.com/exec/obidos/ASIN/B008PXW0C0/amhius20-20\" target=\"_blank\">Seven Principles of Good Government</a><h4>Politics in 2012</h4> Edward Klein - <a href=\"http://www.amazon.com/exec/obidos/ASIN/1596987855/amhius20-20\" target=\"_blank\">The Amateur</a><br>Michael Kranish & Scott Helman - <a href=\"http://www.amazon.com/exec/obidos/ASIN/0062123289/amhius20-20\" target=\"_blank\">The Real Romney</a><br>Lawrence Lessig - <a href=\"http://www.amazon.com/exec/obidos/ASIN/0446576433/amhius20-20\" target=\"_blank\">Republic, Lost: How Money Corrupts Congress--and a Plan to Stop It</a><br>Ron Suskind - <a href=\"http://www.amazon.com/exec/obidos/ASIN/0061429252/amhius20-20\" target=\"_blank\">Confidence Men: Wall Street, Washington, and the Education of a President</a><h4>The Financial Crisis</h4> Johan Norberg - <a href=\"http://www.amazon.com/exec/obidos/ASIN/1935308130/amhius20-20\" target=\"_blank\">Financial Fiasco: How America's Infatuation with Home Ownership and Easy Money Created the Economic Crisis</a><br>John Bellamy Foster & Fred Magdoff - <a href=\"http://www.amazon.com/exec/obidos/ASIN/1583671846/amhius20-20\" target=\"_blank\">The Great Financial Crisis: Causes and Consequences</a>",
+            "has_visits": 1,
+            "no_electoral_majority_image": "https://www.jetsimon.com/public/static/images/2012-no-majority.jpg",
+            "creator": "Ohas",
+            "display_year": "1992 Blue Perot"
+        }
+    }
+]
+
+campaignTrail_temp.candidate_json = [
+    {
+        "model": "campaign_trail.candidate",
+        "pk": 16,
+        "fields": {
+            "first_name": "Ross",
+            "last_name": "Perot",
+            "election": 3,
+            "party": "Democrat",
+            "state": "Texas",
+            "priority": 1,
+            "description": "Eccentric tech billionaire Ross Perot has taken the Democratic Party by storm. The former naval officer and IBM salesman, has propelled himself onto the national stage in no small part due to his populist outsider image.<br><br>Opposition to the Bush administrations free trade proposals, have driven Perot to the Democratic party, a label he's only reluctantly accepted. After a series of interviews and infomercials, he triumphed with ease over many conventional opponents to now be facing HW Bush in the general. Perot's style of unconventional politics are at odds with the bulk of the party establishment, an establishment that's already reluctant to support an uncredentialed independently minded Texas billionaire.<br><br>Perots going to have to market his populist vision to the American public, while also walking through and avoiding the minefield of national politics. If done right, he's assured a spot in history as the man who revolutionized politics, if done wrong, he'll become the scorn of democrats for generations to come.\n\n",
+            "color_hex": "#0000FF",
+            "secondary_color_hex": null,
+            "is_active": 1,
+            "image_url": "https://i.imgur.com/NWxhcC7.png",
+            "electoral_victory_message": "This guy wins!",
+            "electoral_loss_message": "This guy loses!",
+            "no_electoral_majority_message": "We all win?",
+            "description_as_running_mate": null,
+            "candidate_score": 1,
+            "running_mate": false
+        }
+    },
+    {
+        "model": "campaign_trail.candidate",
+        "pk": 31,
+        "fields": {
+            "first_name": "Howell",
+            "last_name": "Heflin",
+            "election": 3,
+            "party": "Democrat",
+            "state": "Alabama",
+            "priority": 1,
+            "description": "<p>Put description here</p>",
+            "color_hex": "#0000FF",
+            "secondary_color_hex": null,
+            "is_active": 0,
+            "image_url": "https://i.imgur.com/HZHjJec.png",
+            "electoral_victory_message": "This guy wins!",
+            "electoral_loss_message": "This guy loses!",
+            "no_electoral_majority_message": "We all win?",
+            "description_as_running_mate": "Howell Heflin is the current sitting senator from Alabama, known for his work as chair of the senate ethics committee, and service as chief justice of the Alabama supreme court. Heflin is generally seen to be a populist conservative democrat, opposing NAFTA, just as you have, but also opposing abortion and gun control. He was also supportive of the Gulf War, while you were vehemently opposed to it. Despite this, Heflins populism matches your own, and if utilized well, could be a boon for your campaign.\n",
+            "candidate_score": 1,
+            "running_mate": true
+        }
+    },
+    {
+        "model": "campaign_trail.candidate",
+        "pk": 32,
+        "fields": {
+            "first_name": "Harris",
+            "last_name": "Wofford",
+            "election": 3,
+            "party": "Democrat",
+            "state": "Pennsylvania",
+            "priority": 1,
+            "description": "\nHarris Wofford is the junior senator from Pennsylvania. Initially appointed by Governor Casey to fill the vacancy left by the death of Senator Heinz, most people expected him to be a placeholder, doubting he could win reelection. Well do that he has, winning a term of his own in 1991. Wofford would make a solid pick, his opposition to NAFTA matches your own, and he could help with mobilizing union support. His position as a democratic food solider would also help woo over establishment types.\n",
+            "color_hex": "#0000FF",
+            "secondary_color_hex": null,
+            "is_active": 0,
+            "image_url": "https://i.imgur.com/DBB0iDg.png",
+            "electoral_victory_message": "This guy wins!",
+            "electoral_loss_message": "This guy loses!",
+            "no_electoral_majority_message": "We all win?",
+            "description_as_running_mate": "\nHarris Wofford is the junior senator from Pennsylvania. Initially appointed by Governor Casey to fill the vacancy left by the death of Senator Heinz, most people expected him to be a placeholder, doubting he could win reelection. Well do that he has, winning a term of his own in 1991. Wofford would make a solid pick, his opposition to NAFTA matches your own, and he could help with mobilizing union support. His service as a democratic foot solider would also help woo over establishment types.\n",
+            "candidate_score": 1,
+            "running_mate": true
+        }
+    },
+    {
+        "model": "campaign_trail.candidate",
+        "pk": 33,
+        "fields": {
+            "first_name": "David",
+            "last_name": "Boren",
+            "election": 3,
+            "party": "Democrat",
+            "state": "Oklahoma",
+            "priority": 1,
+            "description": "Former Governor and senator from Oklahoma David Boren would make a comfortable choice for running mate. While in the senate hes developed a reputation as a fiscally conservative democrat, often crossing party lines to champion tax cuts. In addition he served on the senete select committee on intelligence, bringing some well needed foreign polic credentials to the table. He's also a longtime friend of yours, so theres no doubt the two of you would make for a synergistic ticket. \n",
+            "color_hex": "#0000FF",
+            "secondary_color_hex": null,
+            "is_active": 0,
+            "image_url": "https://i.imgur.com/96X78ov.png",
+            "electoral_victory_message": "This guy wins!",
+            "electoral_loss_message": "This guy loses!",
+            "no_electoral_majority_message": "We all win?",
+            "description_as_running_mate": "David Boren is the former governor, now senator, of Oklahoma, known for regularly crossing party lines to champion across the board tax cuts, and for his service on the Senate select committee on intelligence. A bipartisan legislator, Boren’s selection could help tie together a winning coalition behind you, all the while his foreign policy credentials are a much needed addition. He's also a longtime friend of yours, so there's no doubt the two of you would get along on the campaign trail, something that can't be said about all of your options for running mate. \n\n\n\n",
+            "candidate_score": 1,
+            "running_mate": true
+        }
+    },
+    {
+        "model": "campaign_trail.candidate",
+        "pk": 36,
+        "fields": {
+            "first_name": "Dick",
+            "last_name": "Lamm",
+            "election": 3,
+            "party": "Democrat",
+            "state": "Colorado",
+            "priority": 1,
+            "description": "Dick Lamm, Governor 'Gloom' himself, shares many of your beliefs as well as your eccentricities. A strong fiscal conservative and demagogue against multiculturalism, but also paradoxically a militant environmentalist, he made waves in 1984 after making outspoken statements in favor of physician assisted suicide stating that “We have a duty to die” later going on to make remarks that the old ought to get away from expensive artificial hearts and just die. In this way Lamm is quite like yourself, an outspoken populist, but also an inevitable poison pill for national politics. \n",
+            "color_hex": "#0000FF",
+            "secondary_color_hex": null,
+            "is_active": 0,
+            "image_url": "https://i.imgur.com/qNnlIDd.png",
+            "electoral_victory_message": "This guy wins!",
+            "electoral_loss_message": "This guy loses!",
+            "no_electoral_majority_message": "We all win?",
+            "description_as_running_mate": "Dick Lamm, Governor 'Gloom' himself, shares many of your beliefs as well as your eccentricities. A strong fiscal conservative and demagogue against multiculturalism, but also paradoxically a militant environmentalist, he made waves in 1984 after making outspoken statements in favor of physician assisted suicide stating that “We have a duty to die” later going on to make remarks that the old ought to get away from expensive artificial hearts and just die. In this way Lamm is quite like yourself, an outspoken populist, but also an inevitable poison pill for national politics. \n\n",
+            "candidate_score": 1,
+            "running_mate": true
+        }
+    }
+]
+
+campaignTrail_temp.running_mate_json = [
+    {
+        "model": "campaign_trail.running_mate",
+        "pk": 153261,
+        "fields": {
+            "candidate": 16,
+            "running_mate": 31
+        }
+    },
+    {
+        "model": "campaign_trail.running_mate",
+        "pk": 174376,
+        "fields": {
+            "candidate": 16,
+            "running_mate": 32
+        }
+    },
+    {
+        "model": "campaign_trail.running_mate",
+        "pk": 151371,
+        "fields": {
+            "candidate": 16,
+            "running_mate": 33
+        }
+    },
+    {
+        "model": "campaign_trail.running_mate",
+        "pk": 147885,
+        "fields": {
+            "candidate": 16,
+            "running_mate": 36
+        }
+    }
+]
+
+campaignTrail_temp.credits = "Dan Bryan"
+
+campaignTrail_temp.global_parameter_json = [
+    {
+        "model": "campaign_trail.global_parameter",
+        "pk": 1,
+        "fields": {
+            "vote_variable": 1.125,
+            "max_swing": 0.12,
+            "start_point": 0.94,
+            "candidate_issue_weight": 10,
+            "running_mate_issue_weight": 3,
+            "issue_stance_1_max": -0.71,
+            "issue_stance_2_max": -0.3,
+            "issue_stance_3_max": -0.125,
+            "issue_stance_4_max": 0.125,
+            "issue_stance_5_max": 0.3,
+            "issue_stance_6_max": 0.71,
+            "global_variance": 0.01,
+            "state_variance": 0.005,
+            "question_count": 25,
+            "default_map_color_hex": "#C9C9C9",
+            "no_state_map_color_hex": "#999999"
+        }
+    }
+]
+
+campaignTrail_temp.opponents_default_json = [
+    {
+        "election": 3,
+        "candidates": [
+            16
+        ]
+    }
+]
+
+campaignTrail_temp.opponents_weighted_json = [
+    {
+        "election": 3,
+        "candidates": [
+            16
+        ]
+    }
+]
+
+campaignTrail_temp.temp_election_list = [
+    {
+        "id": 3,
+        "year": 2012,
+        "is_premium": 0,
+        "display_year": "1992 Blue Perot"
+    }
+]
+
+jet_data = {
+    "headerColor": "#700016",
+    "windowColor": "#BFE6FF",
+    "containerColor": "#FFFFFF",
+    "innerWindowColor": "#E8FBFF",
+    "bannerImageUrl": "https://i.imgur.com/LnpU7Xe.jpeg",
+    "backgroundImageUrl": "https://i.imgur.com/nS1OamZ.png",
+    "endingTextColor": "#000000",
+    "customQuote": ""
+}
+
+//#startcode
+
+campaignTrail_temp.musicOn = true
+campaignTrail_temp.musicSrc = "https://audio.jukehost.co.uk/rBUFuedHuLuuVmdWZdy4a6BwbsgF27gN"
+
+
+document.getElementById("music_player").innerHTML = "<button onclick='music_play()'>Play Soundtrack</button>         <button onclick='music_pause()'>Pause</button><br><div style='font-size:0.75em'><input type='radio' name='jukebox' value='song0' checked='' onchange='jukeboxsong()'>Symphony No. 2 - Ludwig van Beethoven</div><audio id='campaigntrailmusic' loop='false' style='display:none' src='' autoplay='false'></audio>"
+
+
+music_play = function() {
+        document.getElementById("campaigntrailmusic").play()
+}
+
+
+music_pause = function() {
+        document.getElementById("campaigntrailmusic").pause()
+}
+
+
+jukeboxsong = function() {
+        campaignTrail_temp.musicSrc = "https://audio.jukehost.co.uk/rBUFuedHuLuuVmdWZdy4a6BwbsgF27gN"
+        document.getElementById('campaigntrailmusic').src = campaignTrail_temp.musicSrc
+}
+
+nct_stuff.themes[nct_stuff.selectedTheme].coloring_title = "#700016";
+
+nct_stuff.themes[nct_stuff.selectedTheme].coloring_window = "#BFE6FF";
+
+document.getElementsByClassName("game_header")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_title;
+
+$("#game_window")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_window;
+
+$(".container")[0].style.backgroundColor = "#FFFFFF";
+
+document.getElementById("header").src = "https://i.imgur.com/LnpU7Xe.jpeg";
+
+document.body.background = "https://i.imgur.com/nS1OamZ.png";
+
+document.head.innerHTML += "<style>#results_container {color:#000000;} .inner_window_w_desc {background-color:#E8FBFF!important;}</style>";
+
+//#endcode
